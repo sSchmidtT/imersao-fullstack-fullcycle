268c551ecdc75349959e3de10acfd7718f88cbd4
sSchmidtT <jonathanschmidt@eucatur.com.br>
2021-02-07 22:11:19 -0400

    Bank-api em Nestjs e Bank-frontend em NextJs implementado.

---
+++ bank-api/src/controllers/my-first/my-first.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('my-first')
+export class MyFirstController {
+
+    @Get("hello-world")
+    index(){
+        return {'key' : 'key'}
+
+    }
+}
